411b9b0385550d35c84391784bf535ca63f89b64
Renato Moreno <renatomorenocz@gmail.com>
2019-09-01 13:28:35 -0300

    ci(test): test codeclimate integration

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,24 @@
 import express from 'express';
 
+function testeDuplicate(arg01: number, arg02: number): number {
+  let newArg = arg01 + arg02;
+  newArg += newArg + arg01 + arg02;
+  return newArg;
+}
+
+function testeDuplicate2(arg01: number, arg02: number): number {
+  let newArg = arg01 + arg02;
+  newArg += newArg + arg01 + arg02;
+  return newArg;
+}
+
 const server = express();
 server.get('/', (_, res) => {
-  res.send('Hello ts-node circle ci test!');
+  res.send('Hello ts-node circle ci test!' + testeDuplicate(0, 3));
 });
 
 server.get('/teste', (_, res) => {
-  res.send('Hello ts-node circle ci test!');
+  res.send('Hello ts-node circle ci test!' + testeDuplicate2(3, 7));
 });
 
 export default server;
