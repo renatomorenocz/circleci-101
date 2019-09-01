8883e188668451c239c17683d4eacdcd086fcaf7
Renato Moreno <renatomorenocz@gmail.com>
2019-08-31 21:33:08 -0300

    feat(express): add server config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Okkkkq");
+import express from "express";
+
+const server = express();
+server.get("/", (_, res) => {
+  res.send("Hello ts-node!");
+});
+
+server.listen(3000, () => {
+  console.log(`[SERVER] Running at http://localhost:3000`);
+});
