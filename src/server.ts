b2b48c6e52aa22f11c2f6569ee3ffb27531e6541
Renato Moreno <renatomorenocz@gmail.com>
2019-09-01 13:33:53 -0300

    ci(codeclimate): remove duplicate code

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,8 @@
 import express from 'express';
 
-function testeDuplicate(arg01: number, arg02: number): number {
-  let newArg = arg01 + arg02;
-  newArg += newArg + arg01 + arg02;
-  return newArg;
-}
-
-function testeDuplicate2(arg01: number, arg02: number): number {
-  let newArg = arg01 + arg02;
-  newArg += newArg + arg01 + arg02;
-  return newArg;
-}
-
 const server = express();
 server.get('/', (_, res) => {
-  res.send('Hello ts-node circle ci test!' + testeDuplicate(0, 3));
-});
-
-server.get('/teste', (_, res) => {
-  res.send('Hello ts-node circle ci test!' + testeDuplicate2(3, 7));
+  res.send('Hello ts-node circle ci test!');
 });
 
 export default server;
